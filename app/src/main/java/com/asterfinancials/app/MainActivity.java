b45ee4b1f892cc3ac823a503d8e4590ca80bfe3a
fundedsquad88-coder1 <funded.squad88@gmail.com>
2026-09-18 17:42:51 +0500

    Remove duplicate legacy MainActivity

--- app/src/main/java/com/asterfinancials/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.asterfinancials.app;
-import android.app.Activity;import android.os.Bundle;import android.webkit.WebSettings;import android.webkit.WebView;import android.webkit.WebViewClient;import android.graphics.Color;
-public class MainActivity extends Activity{
- @Override public void onCreate(Bundle b){super.onCreate(b); getWindow().setStatusBarColor(Color.rgb(5,6,6));getWindow().setNavigationBarColor(Color.rgb(5,6,6)); WebView w=new WebView(this); w.setBackgroundColor(Color.rgb(5,6,6)); w.setWebViewClient(new WebViewClient()); WebSettings s=w.getSettings();s.setJavaScriptEnabled(true);s.setDomStorageEnabled(true);s.setAllowFileAccess(true);s.setAllowContentAccess(true);w.loadUrl("file:///android_asset/index.html");setContentView(w);}
-}
